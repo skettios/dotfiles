054433dd987cbc50f49cca147c8304ec2b8f3202
skettios <brandonjpascual@gmail.com>
2022-09-10 22:31:13 -0700

    remove awesome

--- .config/awesome/main/apps.lua
+++
@@ -1,9 +0,0 @@
-local _apps = {
-    terminal = 'st',
-    launcher = '~/.config/rofi/launchers/type-2/launcher.sh',
-    powermenu = '~/.config/rofi/powermenu/type-2/powermenu.sh',
-    browser = 'firefox'
-}
-
-return _apps
-
